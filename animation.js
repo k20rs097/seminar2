849ad33407a5ce544023d1b1f72564d536f75abc
Tomoya HAYASHI <114045084+k20rs097@users.noreply.github.com>
2024-01-07 23:51:09 +0900

    Update

--- animation.js
+++ animation.js
@@ -1,18 +1,7 @@
-const splashScreen = document.getElementById('splash-screen')
-const ANIMATION_DURATION = 1000
+const topIcon = document.getElementById("top-icon");
+const splashBackgroundColors = ["#000000", "#ff0000", "#0000ff", "#ffdd00", "#f000a0", "#f09000", "#00ee00"];
+let index = 0;
 
-splashScreen.addEventListener ('load', () => {
-    popupAnimation('fade-in', 'block')
-})
-
-function popupAnimation(animation, styleDisplay) {
-  if (styleDisplay === 'block') { changePopupDisplayStyle('block') }
-  historyOverlay.classList.add(animation)
-  historyPopup.classList.add(animation)
-
-  setTimeout(() => {
-    historyOverlay.classList.remove(animation)
-    historyPopup.classList.remove(animation)
-    if (styleDisplay === 'none') { changePopupDisplayStyle('none') }
-  }, ANIMATION_DURATION)
-}
+topIcon.addEventListener( "click", () => {
+  document.documentElement.style.setProperty("--splash-background-color", splashBackgroundColors[index = ++index % splashBackgroundColors.length]);
+});
