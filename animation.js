b356e613c6d7d2537f44c366ea501d1fdcdc792b
Tomoya HAYASHI <114045084+k20rs097@users.noreply.github.com>
2024-01-11 11:18:02 +0900

    Update

--- animation.js
+++ animation.js
@@ -1,7 +1,34 @@
 const topIcon = document.getElementById("top-icon");
+const scrollToContentButton = document.getElementById("scroll-to-content-button");
+const scrollToTopButton = document.getElementById("scroll-to-top-button");
 const splashBackgroundColors = ["#000000", "#ff0000", "#0000ff", "#ffdd00", "#f000a0", "#f09000", "#00ee00"];
+const ANIMATION_DELAY_TIME = 4000;
 let index = 0;
 
-topIcon.addEventListener( "click", () => {
+topIcon.addEventListener("click", () => {
   document.documentElement.style.setProperty("--splash-background-color", splashBackgroundColors[index = ++index % splashBackgroundColors.length]);
 });
+
+scrollToContentButton.addEventListener("click", () => {
+  scrollToElement("content");
+});
+
+scrollToTopButton.addEventListener("click", () => {
+  scrollToElement("hero")
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    scrollToElement("hero");
+  }, ANIMATION_DELAY_TIME);
+});
+
+function scrollToElement(elementId) {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.scrollIntoView( {
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+}
