e8ba4de45adec58afe9b36aa98bbcf35a39f93ed
Tomoya HAYASHI <114045084+k20rs097@users.noreply.github.com>
2024-01-22 15:20:38 +0900

    Update

--- animation.js
+++ animation.js
@@ -1,8 +1,11 @@
+// import anime from "anime.min.js";
+
 // const topIcon = document.getElementById("top-icon");
 const scrollToContentButton = document.getElementById("scroll-to-content-button");
 const scrollToTopButton = document.getElementById("scroll-to-top-button");
 const splashBackgroundColors = ["#000000", "#ff0000", "#0000ff", "#ffdd00", "#f000a0", "#f09000", "#00ee00"];
 const ANIMATION_DELAY_TIME = 4000;
+// const anime = require("animejs");
 let index = 0;
 
 // topIcon.addEventListener("click", () => {
